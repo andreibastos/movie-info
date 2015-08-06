e314c6f235152bb83c69aed8b284c7107b71102a
Andrei Bastos <andreibastos@outlook.com>
2015-08-06 08:45:17 -0300

    remove lines bad of api open subtitles

--- api_open_subtitles.py
+++ api_open_subtitles.py
@@ -9,18 +9,15 @@ import json
 url_base = 'http://www.opensubtitles.org'
 url_search = 'http://www.opensubtitles.org/pb/search/sublanguageid-pob/imdbid-'
 
-
 def get_legend(imdbID):
 	legend = {}
-	
-
 	imdbID = imdbID.strip('tt')
 	print (imdbID)
 	link = url_search + imdbID
 	print (link)
-	
-	link = '/home/andrei/scripts/torrent/imdb/legend.html'
-	link = 'http://www.opensubtitles.org/pb/search/sublanguageid-all/subtrusted-on/hearingimpaired-on/hd-on/autotranslation-on'
+
+	# link = '/home/andrei/scripts/torrent/imdb/legend.html'
+	# link = 'http://www.opensubtitles.org/pb/search/sublanguageid-all/subtrusted-on/hearingimpaired-on/hd-on/autotranslation-on'
 	page = None
 	tree = None
 
@@ -30,10 +27,8 @@ def get_legend(imdbID):
 		page = urllib.urlopen(link)
 		tree = etree.HTML(page.read())
 	except Exception as inst:
-		# print str(inst)
 		return json.dumps({'error':{'code':1,'mensage':str(inst)},'response':response})
 		
-
 	try:
 		for div in tree.xpath("//div[@style='text-align:center']"):
 			for a in div.xpath('h1/a'):
@@ -46,12 +41,9 @@ def get_legend(imdbID):
 		response = True
 		return json.dumps({'response':response,'legend':legend})
 
-
 	except Exception as msg:
-		print msg
 		legends = []
 		try:
-
 			for tr in tree.xpath("//table[@id='search_results']//tr"):
 				# for columns of tr 
 				index_column = 0
@@ -75,28 +67,22 @@ def get_legend(imdbID):
 
 				len_columns = len(tr)
 				legend = None
-				
-				tmp_class = tr.get('class')
-				
+
+				tmp_class = tr.get('class')				
 				if len_columns == 9 and not 'head' in tmp_class:								
-					legend = {}
-					
-					for td in tr.xpath('td'):
-						# print ('\t'+str(index_column))
+					legend = {}					
+					for td in tr.xpath('td'):						
 						if index_column == 0:
 							for a in td.xpath('strong/a'):
-								name_movie =  ((a.text.replace('\n','').replace('\t','')))
-								
-							for img in td.xpath('img'):
-								# print (tostring(img))
+								name_movie =  ((a.text.replace('\n','').replace('\t','')))								
+							for img in td.xpath('img'):								
 								src = img.get('src')
 								if src:						
 									if '/' in src:
 										tmp_src = src.split('/')
 										src = tmp_src[len(tmp_src)-1]
 										if '.' in src:
-											src = src.split('.')[0]	
-											# print src
+											src = src.split('.')[0]									
 										quality_legend.append(src)
 
 							for br in td.xpath("//br"):						
@@ -106,21 +92,13 @@ def get_legend(imdbID):
 							for span in td.xpath('span'):							
 								name_movie_file = span.get('title')
 							
-							
-							# print (name_movie)
-							# print (quality_legend)
-							# print (name_movie_file)
-
 						if index_column == 1:
 							a = td.xpath('a')
 							if a:
 								language = a[0].get('title')						
-							# print (language)
-							# print (tostring(td))
+							
 						if index_column == 2:
-							# print (tostring(td))
 							cd = td.text.replace('\n','').replace('\t','')					
-							# print (cd)
 
 						if index_column == 3:
 							for time in td.xpath('time'):
@@ -128,10 +106,7 @@ def get_legend(imdbID):
 							
 							for span in td.xpath('span'):
 								fps = float(span.text)
-
-							# print (date)
-							# print (fps)
-
+							
 						if index_column == 4:
 							for a in td.xpath('a'):
 								link_download = url_base + a.get('href')
@@ -140,32 +115,22 @@ def get_legend(imdbID):
 							for span in td.xpath('span'):
 								legend_type = span.text
 
-							# print link_download	
-							# print (downloads)
-							# print (legend_type)
 						if index_column  == 5:
 							punctuation = td.text							
 							for img in td.xpath('img'):
 								punctuation = (img.get('src').split('/')[len(img)-1])
 
-							# print (punctuation) 
-
 						if index_column == 6:
 							comments = td.text
-							# print comments
 
 						if index_column == 7:
 							imdbVote = td.xpath('a')[0].text
-							# print(imdbVote)
 
 						if index_column == 8:
 							if len(td.xpath('a'))>0:
 								autor_name = td.xpath('a')[0].text
 							if len(td.xpath('a/img'))>0:						
 								autor_rank = td.xpath('a/img')[0].get('alt')
-
-							# print (autor_name)
-							# print (autor_rank)
 
 						index_column +=1;
 
@@ -184,10 +149,7 @@ def get_legend(imdbID):
 					legend['autor_name'] = autor_name
 					legend['autor_rank'] = autor_rank
 					legends.append(legend)
-					# print(legend)
-					# print ('\n')
-					response = True
-
+					response = True					
 			return json.dumps({'response':response,'legends':legends})
 		
 		except Exception as inst:
@@ -196,9 +158,12 @@ def get_legend(imdbID):
 
 f = open('legendas.json','w')
 
-legenda = get_legend('70219')
+legendas = get_legend('2872718')
 
-print (legenda)
+# leg = json.loads(legendas)
 
-f.write(legenda)
+print (legendas)
+
+f.write(legendas)
+
 f.close
